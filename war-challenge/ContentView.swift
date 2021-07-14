6fe56e467a3fb9a37f8db95f5a6c631077b89f4f
ACGholson <myEmail@.com>
2021-07-13 20:01:00 -0400

    Working war app

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,9 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Define properties to track cards and scores. State properties allow us to change them within the View code. Only this data should be available in this view, so we make these variables private
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card6"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+                
+                // War logo
+                Image("logo")
+                
+                Spacer()
+                
+                // Cards
+                HStack {
+                    
+                    Spacer()
+                    
+                    // Player card on the left
+                    Image(playerCard)
+                    
+                    Spacer()
+                    
+                    // Displays the CPU card on the right
+                    Image(cpuCard)
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                // MARK: Deal button
+                Button(action: {
+                    
+                    // Generate random numbers corresponding to our image assets
+                    let randomPlayerNumber = Int.random(in: 2..<15)
+                    let randomCpuNumber = Int.random(in: 2..<15)
+                    
+                    // Change the cards + score with a button press
+                    playerCard = "card\(randomPlayerNumber)"
+                    cpuCard = "card\(randomCpuNumber)"
+                    
+                    // See who won that round, then update the score.
+                    if randomPlayerNumber > randomCpuNumber {
+                       playerScore += 1
+                    } else if randomPlayerNumber < randomCpuNumber {
+                        cpuScore += 1
+                    }
+    
+                       }) {
+                           Image("dealbutton")
+                           .renderingMode(Image.TemplateRenderingMode?.init(Image.TemplateRenderingMode.original))
+                       }
+                
+                Spacer()
+                
+                // Score labels
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text(String(playerScore)).font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text(String(cpuScore)).font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                
+                
+                Spacer()
+                
+            }.padding()
+            
+        }
     }
 }
 
